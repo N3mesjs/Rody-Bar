6da5e52e37f4a92d81b4a6b77df3ac6dc993a471
N3mesjs <84539974+N3mesjs@users.noreply.github.com>
2025-08-30 17:29:46 +0300

    .

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,26 +1,33 @@
 import Img from 'next/image';
+import React from 'react';
 
-export default function NavBar() {
+interface NavBarProps {
+    ref: React.RefObject<null | HTMLDivElement>;
+}
+
+export default function NavBar(props: NavBarProps) {
+    const scrollToAbout = () => {
+        props.ref.current?.scrollIntoView();
+    };
+
     return (
-        <nav className="flex justify-between items-center p-5 text-white bg-[#F4CE14]">
-            <Img src="/logo.jpeg" alt="Logo" width={60} height={60} />
+        <nav className="flex sticky top-0 justify-between items-center p-5 text-white bg-[#F4CE14]">
+            <Img src="/logo.jpeg" alt="Logo" width={70} height={70} />
             <div className='flex items-center justify-center gap-[3em] text-xl'>
                 <div>
                     <h1 className="font-bold"><a href="/">Rody Bar</a></h1>
                     <hr className="border-2" />
                 </div>
                 <div className='nav-element'>
-                    <a href="/aboutus">Chi Siamo</a>
-                    <hr />
-                </div>
-                <div className='nav-element'>
-                    <a href="">Eventi</a>
+                    <button onClick={scrollToAbout} className="focus:outline-none">
+                         Eventi
+                    </button>
                     <hr />
                 </div>
             </div>
             <div>
-                <a href="/signin" className='px-4 py-3 bg-[#495E57] rounded-2xl inset-ring-3 inset-ring-[#45474B]'>SignIn</a>
+                <a href="/signin" className='px-4 py-3 bg-[#495E57] rounded-2xl shadow'>SignIn</a>
             </div>
         </nav>
-    )
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,37 @@
+"use client"
+
 import Img from 'next/image';
+import { useState, useEffect, useRef } from 'react';
 import NavBar from './components/NavBar';
 
 export default function Page() {
+  const aboutRef = useRef<null | HTMLDivElement>(null);
   return (
     <div className='bg-[#F5F7F8] min-h-screen'>
-      <NavBar />
+      <NavBar ref={aboutRef}/>
+      <main className='flex flex-row mt-[6em]'>
+        <div className='w-1/2 flex flex-col justify-center items-center'>
+        <h1 className='text-4xl font-bold'>Benvenuti al <u>Rody Bar!</u></h1>
+        <div className='w-1/2 text-center mt-5'>
+          <p>Un luogo dove ogni sorriso vale più di mille brindisi. Qui non sei cliente, sei di casa.</p>
+          <button className='mt-5 px-4 py-3 bg-[#495E57] rounded-2xl text-white shadow'>Scopri cosa abbiamo da offrire</button>
+        </div>
+        </div>
+        <div className='w-1/2 flex justify-center'>
+          <Img src="/alex-zie.png" alt="foto del personale" width={400} height={400}/>
+        </div>
+      </main>
+      <section ref={aboutRef} className='mt-[10em] flex flex-col justify-center items-center bg-[#45474B] text-white p-[5em] text-center'>
+        <h2 className='text-4xl font-bold mb-[1em]'>Eventi</h2>
+        <p>
+          Il <strong>Rody Bar</strong> nasce dalla voglia di creare un luogo accogliente,
+          familiare e autentico, dove ogni ospite possa sentirsi a casa.
+        </p>
+        <p>
+          Amiamo la qualità dei nostri caffè, la freschezza dei nostri cocktail
+          e i sorrisi che nascono in ogni serata passata insieme.
+        </p>
+      </section>
     </div>
   )
 }
