ff0c5d076575694033af7a6e11844bbbd9cc96e2
N3mesjs <84539974+N3mesjs@users.noreply.github.com>
2025-09-01 18:13:13 +0200

    aggiunto il menu
    menu cocktail

---
+++ app/menu/page.tsx
@@ -0,0 +1,12 @@
+import NavBar from '../components/NavBar';
+import Img from 'next/image';
+import { useRef } from 'react';
+
+export default function Menu() {
+    return (
+        <div>
+            <NavBar
+        </div>
+
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,25 +4,15 @@ import Img from 'next/image';
 import { useState, useEffect, useRef } from 'react';
 import NavBar from './components/NavBar';
 
-import beer from '/public/beer.png';
-import cockVerde from '/public/cock-verde.png';
-import biraMoretti from '/public/bira-moretti.png';
-import paninoSoppressa from '/public/panino-soppressa.png';
-import spritz from '/public/spritz.png';
-import tagliere from '/public/tagliere.png';
-import toast from '/public/toast.png';
-import caffe from '/public/caffe.png';
-import panino from '/public/panino.png';
-import alexZie from '/public/alex-zie.png';
-import arrow from '/public/arrow.png';
-import logo from '/public/Logo.jpeg';
+import cockV from '../public/cock-verde.png'
 
 export default function Page() {
   const aboutRef = useRef<null | HTMLDivElement>(null);
+  const breakRef = useRef<null | HTMLDivElement>(null);
   return (
     <div className='bg-[#F5F7F8] min-h-screen'>
       <NavBar ref={aboutRef} />
-      <main className='flex flex-row mt-[6em]'>
+      <main className='flex flex-row mt-[6em] w-[1350px]'>
         <div className='w-1/2 flex flex-col justify-center items-center'>
           <h1 className='text-4xl font-bold'>Benvenuti al <u>Rody Bar!</u></h1>
           <div className='w-1/2 text-center mt-5 flex flex-col justify-center items-center'>
@@ -34,7 +24,7 @@ export default function Page() {
           <Img src="/alex-zie.png" alt="foto del personale" width={400} height={400} />
         </div>
       </main>
-      <section ref={aboutRef} className='mt-[10em] flex flex-col justify-center items-center bg-[#45474B] text-white p-[5em] text-center mb-[5em]'>
+      <section ref={aboutRef} className='mt-[6em] flex flex-col justify-center items-center bg-[#45474B] text-white p-[5em] text-center mb-[5em]'>
         <h2 className='text-4xl font-bold mb-[1em]'>Eventi</h2>
         <p>
           Il <strong>Rody Bar</strong> nasce dalla voglia di creare un luogo accogliente,
@@ -45,18 +35,86 @@ export default function Page() {
           e i sorrisi che nascono in ogni serata passata insieme.
         </p>
       </section>
-      <h1 className='text-5xl underline font-bold mb-[2em] text-center'>Le Nostre Pietanze:</h1>
-      <div className="colonne-img border-2 p-5 rounded-lg mb-[7em]">
-        <Img src={beer} alt="birra" />
-        <Img src={cockVerde} alt="cocktail" />
-        <Img src={biraMoretti} alt="birre becks" />
-        <Img src={paninoSoppressa} alt="panino con la soppressa" />
-        <Img src={spritz} alt="vassoio con snack e spritz aperol" />
-        <Img src={tagliere} alt="immagine di un vassoio con snack per un mini evento" />
-        <Img src={toast} alt="toast" />
-        <Img src={caffe} alt="caffe" />
-        <Img src={panino} alt="panino"/>
+      <h2 className='text-5xl underline font-bold mb-[2em] text-center'>Il Nostro Menu:</h2>
+
+      <div className='grid grid-cols-4 gap-[2em] mb-[5em] px-[5em]'>
+        <div className='flex text-black w-[300px] h-max justify-center items-center flex-col'>
+          <Img src={cockV} alt="caffe" className='w-full h-[400px] rounded-2xl mb-[1.5em]' />
+          <button className='text-center font-bold text-2xl mt-2 bg-cyan-600 py-[7px] px-[15px] rounded-2xl shadow button-anim w-full text-white'>Cocktails</button>
+        </div>
+
+        <div className='flex text-black w-[300px] h-max justify-center items-center flex-col'>
+          <Img src={cockV} alt="caffe" className='w-full h-[400px] rounded-2xl mb-[1.5em]' />
+          <button className='text-center font-bold text-2xl mt-2 bg-amber-500 py-[7px] px-[15px] rounded-2xl shadow button-anim w-full text-white'>Cocktails</button>
+        </div>
+
+        <div className='flex text-black w-[300px] h-max justify-center items-center flex-col'>
+          <Img src={cockV} alt="caffe" className='w-full h-[400px] rounded-2xl mb-[1.5em]' />
+          <button className='text-center font-bold text-2xl mt-2 bg-amber-500 py-[7px] px-[15px] rounded-2xl shadow button-anim w-full text-white'>Cocktails</button>
+        </div>
+
+        <div className='flex text-black w-[300px] h-max justify-center items-center flex-col'>
+          <Img src={cockV} alt="caffe" className='w-full h-[400px] rounded-2xl mb-[1.5em]' />
+          <button className='text-center font-bold text-2xl mt-2 bg-amber-500 py-[7px] px-[15px] rounded-2xl shadow button-anim w-full text-white'>Cocktails</button>
+        </div>
       </div>
+
+      <section className='bg-[#495E57] text-white p-[7em]'>
+        <div ref={breakRef} className='colonne text-2xl'>
+          <div className='flex flex-col gap-[1.5em]'>
+            <div>
+              <Img src="/spritz.png" alt="caffè" width={300} height={300} />
+            </div>
+            <div>
+              <Img src="/cock-verde.png" alt="cocktail verde" width={300} height={300} />
+            </div>
+          </div>
+          <div className='flex flex-col gap-[1.5em]'>
+            <h2 className='text-4xl'>Cocktail</h2>
+            <hr />
+            <div className='flex justify-between'>
+              <h3 className='font-medium'>Hugo</h3>
+              <span>€ 6,00</span>
+            </div>
+            <div className='flex justify-between'>
+              <h3 className='font-medium'>Mojito</h3>
+              <span>€ 6,00</span>
+            </div>
+            <div className='flex justify-between'>
+              <h3 className='font-medium'>Long Island Ice Tea</h3>
+              <span>€ 6,00</span>
+            </div>
+            <div className='flex justify-between'>
+              <h3 className='font-medium'>Americano</h3>
+              <span>€ 6,00</span>
+            </div>
+            <div className='flex justify-between'>
+              <h3 className='font-medium'>Negroni</h3>
+              <span>€ 6,00</span>
+            </div>
+            <div className='flex justify-between'>
+              <h3 className='font-medium'>Pina Colada</h3>
+              <span>€ 6,00</span>
+            </div>
+            <div className='flex justify-between'>
+              <h3 className='font-medium'>Coca e Malibù</h3>
+              <span>€ 6,00</span>
+            </div>
+            <div className='flex justify-between'>
+              <h3 className='font-medium'>Coca Havana</h3>
+              <span>€ 6,00</span>
+            </div>
+            <div className='flex justify-between'>
+              <h3 className='font-medium'>Gin Tonic</h3>
+              <span>€ 6,00</span>
+            </div>
+            <div className='flex justify-between'>
+              <h3 className='font-medium'>Gin Lemon</h3>
+              <span>€ 6,00</span>
+            </div>
+          </div>
+        </div>
+      </section>
       <footer>
         sfws
       </footer>
