d7ce41918c8f4f775ba36a66d6051af6bf810fa2
N3mesjs <84539974+N3mesjs@users.noreply.github.com>
2025-09-10 16:53:24 +0200

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import './global.css'
 
+import type { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'Rody Bar Caffe',
+  description: 'Rody Bar Caffe, sito web ufficiale del bar di Rodica, Alex e Ana',
+}
+
 export default function RootLayout( { children }: { children: React.ReactNode } ) {
     return (
         <html lang="en">
